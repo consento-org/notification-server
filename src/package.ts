8bf8caf7ae2fc845348c94f106117fe77f0f8961
Martin Heidegger <martin.heidegger@gmail.com>
2020-10-13 16:11:24 +0900

    v0.1.19

--- src/package.ts
+++ src/package.ts
@@ -1,3 +1,3 @@
 // Using this directly from package.json breaks the release task
 export const NAME = '@consento/notification-server'
-export const VERSION = '0.1.18'
+export const VERSION = '0.1.19'
